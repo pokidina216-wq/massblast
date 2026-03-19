e00533c976d1b2f284f5c76b6c1651c80bff2326
stmp <pokidina216@gmail.com>
2026-03-20 00:40:29 +0200

    Add files via upload

--- massblast-server/server.js
+++ massblast-server/server.js
@@ -6,7 +6,7 @@ const path     = require('path');
 const crypto   = require('crypto');
 
 const app  = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
